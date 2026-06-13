42b32a80f3552c2948c9bfa3f73717ecdf4e3e74
FrejaLD <itsfreja@hotmail.com>
2026-06-13 23:07:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+// Get the button
+let mybutton = document.getElementById("backToTopBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// When the user scrolls down 300px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    mybutton.style.display = "block";
+    mybutton.classList.add("show");
   } else {
-    mybutton.style.display = "none";
+    mybutton.classList.remove("show");
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+mybutton.addEventListener("click", function(){
+  window.scrollTo({top: 0, behavior: 'smooth'});
+});
