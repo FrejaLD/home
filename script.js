14e26dd3982be671cf22db6d14c903bad0472d41
FrejaLD <itsfreja@hotmail.com>
2026-06-13 22:02:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+// Check distance to top and display back-to-top.
+$( window ).scroll( function() {
+	if ( $( this ).scrollTop() > 800 ) {
+		$( '.back-to-top' ).addClass( 'show-back-to-top' );
+	} else {
+		$( '.back-to-top' ).removeClass( 'show-back-to-top' );
+	}
+});
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// Click event to scroll to top.
+$( '.back-to-top' ).click( function() {
+	$( 'html, body' ).animate( { scrollTop : 0 }, 800 );
+	return false;
+});
